0e494f37fd83bb76fb4e7c3214c41556a0240dfc
cgarz <20268068+cgarz@users.noreply.github.com>
2022-03-23 01:56:59 +0000

    integrate WA_Flags into WA_discord Now that its much shorter and only referenced in WA_Discord Also switch to :triangular_flag_on_post: for id 49 and default

--- wa_discord.py
+++ wa_discord.py
@@ -6,10 +6,21 @@ from datetime import datetime, timezone
 
 import discord
 
-from wa_flags import WA_Flags
-
 
 class WA_Discord(discord.Client):
+    WA_Flags = {  # https://worms2d.info/WormNET_flags
+         '0': ':flag_gb:',  '1': ':flag_ar:',  '2': ':flag_au:',  '3': ':flag_at:',  '4': ':flag_be:',
+         '5': ':flag_br:',  '6': ':flag_ca:',  '7': ':flag_hr:',  '8': ':flag_ba:',  '9': ':flag_cy:',
+        '10': ':flag_cz:', '11': ':flag_dk:', '12': ':flag_fi:', '13': ':flag_fr:', '14': ':flag_ge:',
+        '15': ':flag_de:', '16': ':flag_gr:', '17': ':flag_hk:', '18': ':flag_hu:', '19': ':flag_is:',
+        '20': ':flag_in:', '21': ':flag_id:', '22': ':flag_ir:', '23': ':flag_iq:', '24': ':flag_ie:',
+        '25': ':flag_il:', '26': ':flag_it:', '27': ':flag_jp:', '28': ':flag_li:', '29': ':flag_lu:',
+        '30': ':flag_my:', '31': ':flag_mt:', '32': ':flag_mx:', '33': ':flag_ma:', '34': ':flag_nl:',
+        '35': ':flag_nz:', '36': ':flag_no:', '37': ':flag_pl:', '38': ':flag_pt:', '39': ':flag_pr:',
+        '40': ':flag_ro:', '41': ':flag_ru:', '42': ':flag_sg:', '43': ':flag_za:', '44': ':flag_es:',
+        '45': ':flag_se:', '46': ':flag_ch:', '47': ':flag_tr:', '48': ':flag_us:', '49': ':triangular_flag_on_post:'
+    }
+
     def __init__(self, token: str, guilds: dict):
         # internal properties
         self.token = token  # discord token used for authenticating bot
@@ -28,7 +39,7 @@ class WA_Discord(discord.Client):
         self.embed_color = 0xffa300
         self.embed_icon = 'https://cdn.discordapp.com/emojis/501802399565086720.png?size=32'
         self.embed_footer = 'List last refreshed at'
-        self.embed_default_flag = ':checkered_flag:'
+        self.embed_default_flag = self.WA_Flags['49']
         self.embed_public_game = ':unlock:'
         self.embed_private_game = ':closed_lock_with_key:'
         self.embed_no_host = '*There are currently no games hosted on WormNet..* <:sadworm:883155422675087452>'
